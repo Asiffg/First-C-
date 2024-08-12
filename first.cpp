73d6ce2b449dcee395ebd85ba8b9f0b0acb4f51f
Asiffg <Asiffgei1999@gmail.com>
2024-08-11 21:08:38 -0700

    this is updated C++ Programme

---
+++ first.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+int main(){
+    // normal printing
+    std ::cout << "I like programming\n";
+    std ::cout << "I like pizza\n";
+
+    // integer datatypes("Whole Numbers")
+    int x=3;
+    int y=5;
+    int sum=x+y;
+
+    // char datatypes("Single letter")
+    char alpha='A';
+    char beta='B';
+    char dollar='$';
+
+    // string datatypes ("Object that represents a sequence of text characters")
+    std::string name="Asif";
+    std::string age="Five";
+    std::string address="block A Pakistan";
+    
+    // double datatypes("decimal numbers")
+    double temperature=37.5;
+    double price=305.9;
+    double size=2.5;
+
+    // boolean datatypes("true or false")
+    bool schoolleave=true;
+    bool schoolleave=false;
+    bool open=true;
+    
+    return 0;
